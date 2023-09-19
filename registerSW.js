407c7825a0910a43db1b639f4b721a29f4f3bf49
Sanadhi2002 <sanadhi2002@gmail.com>
2023-09-20 00:13:17 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/crypto-pwa/sw.js', { scope: '/crypto-pwa/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
